b8c93e09b6e694645649cc432531d1f195b007ad
Matheus Nunes <matheus.nunes@poli.digital>
2026-03-11 16:50:27 -0300

    add checkout scenario

---
+++ cypress/e2e/checkout.cy.ts
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker'
+
+describe('Login scenarios', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.login(Cypress.env('standard_user'), Cypress.env('password'))
+  })
+
+  it('should complete the checkout process successfully', () => {
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const postalCode = faker.location.zipCode()
+
+    cy.getBySel('add-to-cart-sauce-labs-backpack').click()
+    cy.getBySel('shopping-cart-link').click()
+    cy.getBySel('checkout').click()
+    cy.getBySel('firstName').type(firstName)
+    cy.getBySel('lastName').type(lastName)
+    cy.getBySel('postalCode').type(postalCode)
+    cy.getBySel('continue').click()
+    cy.getBySel('finish').click()
+
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/checkout-complete.html`)
+    cy.getBySel('title').should('have.text', 'Checkout: Complete!')
+    cy.getBySel('complete-header').should('have.text', 'Thank you for your order!')
+  })
+})
