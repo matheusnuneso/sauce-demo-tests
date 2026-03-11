ba177ef33ce5d6a2f469c9eb826356e487cd9133
Matheus Nunes <matheus.nunes@poli.digital>
2026-03-11 18:36:47 -0300

    add assertions to error msgs in checkout scenarios

--- cypress/e2e/checkout.cy.ts
+++ cypress/e2e/checkout.cy.ts
@@ -1,15 +1,15 @@
 import { faker } from '@faker-js/faker'
 
 describe('Checkout scenarios', () => {
+  const firstName = faker.person.firstName()
+  const lastName = faker.person.lastName()
+  const postalCode = faker.location.zipCode()
+
   beforeEach(() => {
     cy.loginViaCookie(Cypress.env('standard_user'))
   })
 
   it('should complete the checkout process successfully', () => {
-    const firstName = faker.person.firstName()
-    const lastName = faker.person.lastName()
-    const postalCode = faker.location.zipCode()
-
     cy.getBySel('add-to-cart-sauce-labs-backpack').click()
     cy.getBySel('shopping-cart-link').click()
     cy.getBySel('checkout').click()
@@ -22,5 +22,33 @@ describe('Checkout scenarios', () => {
     cy.url().should('contain', '/checkout-complete.html')
     cy.getBySel('title').should('have.text', 'Checkout: Complete!')
     cy.getBySel('complete-header').should('have.text', 'Thank you for your order!')
+  })
+
+  describe('Validate errors messages', () => {
+
+    beforeEach(() => {
+      cy.visit('/checkout-step-one.html', { failOnStatusCode: false })
+    })
+
+    it('should show error when first name is missing', () => {
+      cy.getBySel('continue').click()
+
+      cy.checkErrorMsg('Error: First Name is required')
+    })
+
+    it('should show error when last name is missing', () => {
+      cy.getBySel('firstName').type(firstName)
+      cy.getBySel('continue').click()
+
+      cy.checkErrorMsg('Error: Last Name is required')
+    })
+
+    it('should show error when Postal Code is missing', () => {
+      cy.getBySel('firstName').type(firstName)
+      cy.getBySel('lastName').type(lastName)
+      cy.getBySel('continue').click()
+
+      cy.checkErrorMsg('Error: Postal Code is required')
+    })
   })
 })
